1a1f2eee3280758503221ddb33a73968c882549d
Mykhailo Pysariev <m.pysarev@gmail.com>
2020-06-17 21:25:22 +0300

    HT 14 modified, HT 14 extra task added

--- HT 14 extra task/script.js
+++ HT 14 extra task/script.js
@@ -4,13 +4,15 @@ const listContainer = document.createElement('ul');
 
 const CLASS_CONTAINER = 'container';
 const CLASS_LI = 'li';
+const CLASS_DELETE = 'delete';
 const CLASS_COMPLETE = 'complete';
 
 document.body.append(listContainer);
 
 listContainer.classList.add(CLASS_CONTAINER);
 
-
+listContainer.addEventListener('click', toggleElement);
+listContainer.addEventListener('click', deleteElement);
 
 
 fetch('https://jsonplaceholder.typicode.com/todos')
@@ -30,18 +32,24 @@ function getData(dataArr) {
 function generateHtml(dataObj) {
 
     const todoEl = document.createElement('li');
+    const deleteBtn = document.createElement('span');
+
 
     todoEl.textContent = dataObj.title;
+    deleteBtn.textContent = 'X';
+
     
     listContainer.append(todoEl);
+    todoEl.append(deleteBtn);
 
-    addClass(todoEl, dataObj, todoEl);    
+    addClass(todoEl, dataObj, todoEl, deleteBtn);    
 }
 
 
 
-function addClass(task, obj, li) {
+function addClass(task, obj, li, deleteBtn) {
     task.classList.add(CLASS_LI);
+    deleteBtn.classList.add(CLASS_DELETE);
 
     checkStatus(obj, li);
 }
@@ -51,4 +59,14 @@ function checkStatus(obj, li) {
     if(obj.completed) {
         li.classList.add(CLASS_COMPLETE);
     } 
+}
+
+function deleteElement(e) {
+    if(e.target.classList.contains(CLASS_DELETE)) {
+        e.target.parentNode.remove();
+    }
+}
+
+function toggleElement(e) {
+    e.target.classList.toggle(CLASS_COMPLETE);
 }
